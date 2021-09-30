8c959ac10fb425b6c7c11649b2e76c35ac85eb7a
Juan Gil <juank.gv@live.com>
2021-09-29 20:35:51 -0500

    primer commit

---
+++ src/main/java/edu/eci/arsw/transportari/TransportariApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.transportari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/transportari/TransportariApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.transportari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
